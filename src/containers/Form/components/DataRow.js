0d54cdfda538dc26b4b017b90067d648f8736120
Bhupali Maheshwari <bmaheshwari@deqode.com>
2021-06-14 18:38:50 +0530

    Added edit delete button

--- src/containers/Form/components/DataRow.js
+++ src/containers/Form/components/DataRow.js
@@ -7,6 +7,8 @@ function DataRow(props) {
 			<td>{props.gender}</td>
 			<td>{props.edu}</td>
 			<td><img className="box-image" src={props.pic} alt='' /></td>
+			<td><button name="edit" onClick={props.handleAction}>Edit</button></td>
+			<td><button name="delete" onClick={props.handleAction}>Delete</button></td>
 		</tr>
 	);
 }
